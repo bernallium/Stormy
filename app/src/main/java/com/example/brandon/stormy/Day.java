da73a355b2fcf95de374ce01a6c4cdf0433275e3
Brandon Bernal <bernal.brandon@gmail.com>
2017-01-07 14:35:40 -0500

    Create Day class

---
+++ app/src/main/java/com/example/brandon/stormy/Day.java
@@ -0,0 +1,49 @@
+package com.example.brandon.stormy;
+
+public class Day {
+    private long mTime;
+    private String mSummary;
+    private double mTemperatureMax;
+    private String mIcon;
+    private String mTimezone;
+
+    public long getTime() {
+        return mTime;
+    }
+
+    public void setTime(long time) {
+        mTime = time;
+    }
+
+    public String getSummary() {
+        return mSummary;
+    }
+
+    public void setSummary(String summary) {
+        mSummary = mSummary;
+    }
+
+    public double getTemperatureMax() {
+        return mTemperatureMax;
+    }
+
+    public void setmTemperatureMax(double temperatureMax) {
+        mTemperatureMax = temperatureMax;
+    }
+
+    public String getmIcon() {
+        return mIcon;
+    }
+
+    public void setIcon(String icon) {
+        mIcon = icon;
+    }
+
+    public String getTimezone() {
+        return mTimezone;
+    }
+
+    public void setTimezone(String timezone) {
+        mTimezone = mTimezone;
+    }
+}
