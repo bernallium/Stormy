a2662f870aaf767610f77c8778364e9924b850a2
Brandon Bernal <bernal.brandon@gmail.com>
2017-01-30 02:27:33 -0500

    Create an Adapter to adapt the data for the ListView.

--- app/src/main/java/com/example/brandon/stormy/ui/DailyForecastActivity.java
+++ app/src/main/java/com/example/brandon/stormy/ui/DailyForecastActivity.java
@@ -2,6 +2,7 @@ package com.example.brandon.stormy.ui;
 
 import android.app.ListActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 
 import com.example.brandon.stormy.R;
 
@@ -13,5 +14,7 @@ public class DailyForecastActivity extends ListActivity {
         setContentView(R.layout.activity_daily_forecast);
 
         String[] daysOfTheWeek = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, daysOfTheWeek);
+        setListAdapter(adapter),
     }
 }
