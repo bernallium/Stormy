e88cb379ca67190649e97069d51eec183e34c446
Brandon Bernal <bernal.brandon@gmail.com>
2017-03-03 16:02:58 -0500

    Add view for the daily weather.

--- app/src/main/java/com/example/brandon/stormy/ui/DailyForecastActivity.java
+++ app/src/main/java/com/example/brandon/stormy/ui/DailyForecastActivity.java
@@ -14,7 +14,15 @@ public class DailyForecastActivity extends ListActivity {
         setContentView(R.layout.activity_daily_forecast);
 
         String[] daysOfTheWeek = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, daysOfTheWeek);
-        setListAdapter(adapter),
+
+        /* ArrayAdapter is a generic, meaning that the type of the items in the array needs to be specified in <angle brackets>.
+        The constructor takes three parameters:
+            1) the context (use this),
+            2) the layout (use android.R.layout.simple_list_item_1)
+            3) the array to adapt
+         */
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String> (this, android.R.layout.simple_list_item_1, daysOfTheWeek);
+        setListAdapter(adapter);
     }
 }
